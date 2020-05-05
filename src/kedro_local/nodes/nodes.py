31dff851693c36e812d0396829a84706a0b2bc2f
hrappuccino <hrappuccino@gmail.com>
2020-05-05 14:57:25 +0900

    Notebook pipeline example using Papermill and YAML

---
+++ src/kedro_local/nodes/nodes.py
@@ -0,0 +1,23 @@
+import papermill as pm
+import os, re, json, hashlib, datetime
+
+def _extract_dataset_name_from_log(output_text):
+    m = re.search(r'kedro.io.data_catalog - INFO - Saving data to `(\w+)`', output_text)
+    return m.group() if m else None
+
+class NotebookExecuter:
+    def __init__(self, input_path, output_path=None, parameters=None):
+        self.__input_path = input_path
+        self.__parameters = parameters
+        self.__output_path = output_path or self.__get_default_output_path()
+
+    def __call__(self, *args):
+        nb = pm.execute_notebook(self.__input_path, self.__output_path, self.__parameters)
+        dataset_names = [_extract_dataset_name_from_log(output['text']) for cell in nb['cells'] for output in cell['outputs']]
+        return [dataset_name for dataset_name in dataset_names if dataset_name]
+
+    def __get_default_output_path(self):
+        name, ext = os.path.splitext(os.path.basename(self.__input_path))
+        parameters_hash = hashlib.sha1(json.dumps(self.__parameters, sort_keys=True).encode('utf-8')).hexdigest()
+        timestamp = datetime.datetime.now().isoformat()
+        return os.path.join(os.environ['PAPERMILL_OUTPUT_DIR'], f'{name}_{parameters_hash}_{timestamp}{ext}')
--- src/kedro_notebook_project/pipeline.py
+++ src/kedro_notebook_project/pipeline.py
@@ -1,68 +1,26 @@
-# Copyright 2020 QuantumBlack Visual Analytics Limited
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND
-# NONINFRINGEMENT. IN NO EVENT WILL THE LICENSOR OR OTHER CONTRIBUTORS
-# BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN
-# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF, OR IN
-# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-# The QuantumBlack Visual Analytics Limited ("QuantumBlack") name and logo
-# (either separately or in combination, "QuantumBlack Trademarks") are
-# trademarks of QuantumBlack. The License does not grant you any right or
-# license to the QuantumBlack Trademarks. You may not use the QuantumBlack
-# Trademarks or any confusingly similar mark as a trademark for your product,
-#     or use the QuantumBlack Trademarks in any other manner that might cause
-# confusion in the marketplace, including but not limited to in advertising,
-# on websites, or on software.
-#
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from kedro.pipeline import Pipeline, node
+from kedro_local.nodes import *
+import yaml
 
-"""Construction of the master pipeline.
-"""
+def create_pipelines(**kwargs):
+    with open('conf/base/pipelines.yaml') as f:
+        pipelines_ = yaml.safe_load(f)
 
-from typing import Dict
-from kedro.pipeline import Pipeline
-
-
-
-###########################################################################
-# Here you can find an example pipeline, made of two modular pipelines.
-#
-# Delete this when you start working on your own Kedro project as
-# well as pipelines/data_science AND pipelines/data_engineering
-# -------------------------------------------------------------------------
-
-from kedro_notebook_project.pipelines import data_engineering as de
-from kedro_notebook_project.pipelines import data_science as ds
-
-
-def create_pipelines(**kwargs) -> Dict[str, Pipeline]:
-    """Create the project's pipeline.
-
-    Args:
-        kwargs: Ignore any additional arguments added in the future.
-
-    Returns:
-        A mapping from a pipeline name to a ``Pipeline`` object.
-
-    """
-
-
-    data_engineering_pipeline = de.create_pipeline()
-    data_science_pipeline = ds.create_pipeline()
-
-    return {
-        "de": data_engineering_pipeline,
-        "ds": data_science_pipeline,
-        "__default__": data_engineering_pipeline + data_science_pipeline,
+    pipelines = {
+        pipeline_name: Pipeline([
+            node(
+                NotebookExecuter(**node_['nb']),
+                node_['inputs'],
+                node_['outputs'],
+                name=node_name,
+            ) for node_name, node_ in nodes_.items()
+        ]) for pipeline_name, nodes_ in pipelines_.items()
     }
 
+    for pipeline_ in list(pipelines.values()):
+        if '__default__' not in pipelines:
+            pipelines['__default__'] = pipeline_
+        else:
+            pipelines['__default__'] += pipeline_
+
+    return pipelines
