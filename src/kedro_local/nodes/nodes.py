8be578fa6b06f050b58540f836f9b2de8dfac295
hrappuccino <hrappuccino@gmail.com>
2020-05-14 08:25:04 +0900

    Allowed cell['outputs'] and output['text'] of notebooks to be None

--- src/kedro_local/nodes/nodes.py
+++ src/kedro_local/nodes/nodes.py
@@ -19,7 +19,11 @@ class NotebookExecuter:
 
     def __call__(self, *args):
         nb = pm.execute_notebook(self.__input_path, self.__output_path, self.__parameters)
-        dataset_names = [_extract_dataset_name_from_log(output['text']) for cell in nb['cells'] for output in cell['outputs']]
+        dataset_names = [
+            _extract_dataset_name_from_log(output['text'])
+            for cell in nb['cells'] if 'outputs' in cell
+            for output in cell['outputs'] if 'text' in output
+        ]
         return [self.__catalog.load(dataset_name) for dataset_name in dataset_names if dataset_name]
 
     def __get_default_output_path(self):
